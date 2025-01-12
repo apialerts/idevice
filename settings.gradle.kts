5b1672bf927e3b5ffc4fadb11f208d7e782a41e3
Jared Hall <jared@apialerts.com>
2025-01-12 16:20:22 +1100

    java 17

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "iDevice"
 include(":library")
+
+val javaVersion = System.getProperty("java.version")?.split(".")?.firstOrNull()?.toInt() ?: Int.MAX_VALUE
+
+val requiredJava = JavaVersion.VERSION_17.toString().toInt()
+check (javaVersion >= requiredJava) {
+    "This project needs to be run with Java $requiredJava or higher (found: $javaVersion)."
+}
