e3cfb973a306eb845b09917cbfeaff79cc884abe
Jared Hall <jared@apialerts.com>
2025-01-12 22:59:35 +1100

    formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform) apply  false
+    alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
